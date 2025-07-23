7d5edd17b27650abc5ca6f57aebdc41e268f7ace
Biswabhusan pradhan <biswabhusanpradhan0588@gmail.com>
2025-07-23 08:39:40 +0530

    1st commit

---
+++ day2/script for variable.js
@@ -0,0 +1,60 @@
+let username = 'Biswa' 
+console.log(username)
+
+let AGE = 25
+console.log(AGE)
+console.log(typeof AGE)
+
+let isHappy = true
+console.log(isHappy)
+console.log(typeof isHappy)
+
+let lastName = 'pradhan'
+console.log(lastName)
+console.log(typeof lastName)
+
+let userIntro = "hii i am "+ username +""+ lastName + " i am "+ AGE + " years old"
+console.log(userIntro)
+let a = undefined
+console.log(a)
+console.log(typeof a)
+
+let b;
+console.log(b)
+console.log(typeof b)
+
+b = false
+console.log (b, typeof b ,'xyz')
+
+let c = null
+console.log(c , typeof c)
+
+const hoursInDay = 20
+var e 
+e = 10;
+console.log(e)
+
+var f 
+console.log(f)
+
+{
+    var g = 200
+    let h = 30;
+}
+console.log(g)
+// console.log(h)
+
+// console.log(x)
+let x = 2020;
+console.log(x)
+
+console.log('before initialion the value of y',y)// undefined
+var y = 3020
+console.log(y)
+
+
+
+
+
+
+
