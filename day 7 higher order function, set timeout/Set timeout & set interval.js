26935cae7e394d6e2492003fb0c687ef93030a82
Biswabhusan pradhan <biswabhusanpradhan0588@gmail.com>
2025-07-30 08:02:01 +0530

    arrow function.js

---
+++ day 7 higher order function, set timeout/Set timeout & set interval.js
@@ -0,0 +1,12 @@
+// console.log(1)
+
+// setTimeout(hello,2000)
+
+function hello(){
+    console.log("HELLO EVERYONE!!")
+}
+// console.log(3)
+//  const id = setInterval(hello,2000)
+//  console.log(id)
+
+console.log(setInterval(hello,2000))
---
+++ day 7 higher order function, set timeout/arrow function.js
@@ -0,0 +1,16 @@
+const sayHello = (fullName) =>{
+ console.log( fullName,"good morning everyone")
+
+}
+    sayHello(fullName)
+
+
+// const square = n=> console.log(n*n)
+square(3)
+
+const square = n=> n * n;
+const square2 = n=> {
+    return n*n;
+}
+console.log(square(3)) 
+console.log(square2(5))
